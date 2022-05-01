0232ec63015baf606dc1184e52dc71c16fbf9b89
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-05-01 13:09:31 +0530

    x=int(input()) for i in range(1,x+1):     for j in range(1,x+1):         if i==j:             print("0",end='')         else:             print("x",end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 x=int(input())
-for i in range(x,0,-1):
-    for j in range(1,i+1):
-        print(j,end='')
+for i in range(1,x+1):
+    for j in range(1,x+1):
+        if i==j:
+            print("0",end='')
+        else:
+            print("x",end='')
     print()
