7106acf2bb693d2978da4fe5eaa07de85f4950cf
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-05-30 21:32:49 +0530

    n=int(input()) k=65+n-1 l=n for i in range(1,n+1):     for j in range(1,l+1):         print(chr(k),end=' ')     k-=1     l-=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,9 @@
-k=65
 n=int(input())
+k=65+n-1
+l=n
 for i in range(1,n+1):
-    for j in range(1,n+1):
+    for j in range(1,l+1):
         print(chr(k),end=' ')
-    k+=1
+    k-=1
+    l-=1
     print()
