e9d9724da8cc1bc753ea9971553fe2029c2d344d
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-06-10 11:02:19 +0530

    n=input() n=n.split() for i in range(len(n)):     if i==len(n)-1:         k=list(n[i])         for j in range(len(k)):             if j==0:                 print(k[j])

---
+++ last_word_first_character.py
@@ -0,0 +1,11 @@
+n=input()
+n=n.split()
+for i in range(len(n)):
+    if i==len(n)-1:
+        k=list(n[i])
+        for j in range(len(k)):
+            if j==0:
+                print(k[j])
+            
+                
+        
