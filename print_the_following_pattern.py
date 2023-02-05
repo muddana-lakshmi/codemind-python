31c7f5dce1be310d543c78759d593d100b310e11
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2023-02-05 10:26:09 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,n-2+1):         print(j,end='')     for j in range(n-3,0,-1):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,7 +2,7 @@ n=int(input())
 for i in range(1,n+1):
     for j in range(1,n-2+1):
         print(j,end='')
-    for j in range(1,n-3+1):
+    for j in range(n-3,0,-1):
         print(j,end='')
     print()
         
